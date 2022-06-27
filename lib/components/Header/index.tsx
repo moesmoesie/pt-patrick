389c1c3001fb05d0bd58a89b6fac3f3255400d16
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-06-28 00:35:06 +0200

    Add z index to header

--- lib/components/Header/index.tsx
+++ lib/components/Header/index.tsx
@@ -3,7 +3,7 @@ import Container from "../Container";
 
 const Header: React.FC<HeaderProps> = () => {
   return (
-    <div className="w-full bg-white fixed top-0 left-0">
+    <div className="w-full z-[999] bg-white fixed top-0 left-0">
       <div className="flex border-b border-b-gray-300 px-2 py-5">
         <img className="h-6" src="/assets/logo.png" alt="logo" />
       </div>
