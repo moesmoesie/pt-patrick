58fd8d84debd362ce2c8373299b1464e185ab21d
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-06-27 15:12:38 +0200

    Initial commit

---
+++ pages/_document.tsx
@@ -0,0 +1,43 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        <link
+          rel="preload"
+          href={`/fonts/rubik/rubik-v20-latin-regular.woff2`}
+          as="font"
+          crossOrigin=""
+        />
+
+        <link
+          rel="preload"
+          href={`/fonts/rubik/rubik-v20-latin-700.woff2`}
+          as="font"
+          crossOrigin=""
+        />
+
+        <link
+          rel="preload"
+          href={`/fonts/overpass/overpass-v11-latin-regular.woff2`}
+          as="font"
+          crossOrigin=""
+        />
+
+        <link
+          rel="preload"
+          href={`/fonts/overpass/overpass-v11-latin-700.woff2`}
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      medium: "768px",
+      large: "1280px",
+    },
+    fontFamily: {
+      rubik: ["Rubik", "sans"],
+      overpass: ["Overpass", "sans-serif"],
+    },
+    colors: {
+      green: "#4ae290",
+      black: "#000000",
+      white: "#ffffff",
+      amethyst: "#7b61ff",
+      "gray-300": "#f0f0f0",
+      "gray-400": "#eeeeee",
+      "gray-500": "#cccccc",
+      "gray-900)": "#333333",
+    },
+    text: {},
+    extend: {},
+  },
+  plugins: [],
+};
