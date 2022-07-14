d028554dfb6c4a2318791cd2086a66bf70520bc2
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-07-14 21:50:05 +0200

    Fix Home workout module bug

--- lib/modules/HomeWorkoutsModule/index.tsx
+++ lib/modules/HomeWorkoutsModule/index.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const HomeWorkoutsModule: React.FC<HomeWorkoutsModuleProps> = (props) => {
   return (
-    <ModuleContainer color="#f0f0f0">
+    <ModuleContainer paddingBottom="large" color="#f0f0f0">
       <div>
         <h2 className="header-2 font-bold mb-12">{props.title}</h2>
         <div className="flex w-full gap-4 pb-4 overflow-x-scroll">
