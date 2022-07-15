0995222e9cb5b090353a30d46c1931bcdd0da94d
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-07-15 21:48:53 +0200

    Add blurred background to card on browsers that support it

--- lib/components/Card/index.tsx
+++ lib/components/Card/index.tsx
@@ -2,9 +2,9 @@ import CardProps from "./types";
 
 const Card: React.FC<CardProps> = (props) => {
   return (
-    <div className="px-4 py-3 medium:px-5 medium:py-6 bg-white shadow-1 rounded-[20px] border border-amethyst">
+    <div className="px-4 py-3 supports-backdrop-filter:bg-white/60 supports-backdrop-filter:backdrop-blur-lg large:px-5 large:py-6 bg-white shadow-1 rounded-[20px] border border-amethyst">
       <div className="flex items-center mb-4">
-        <div className="text-amethyst large:text-[20px] text-[16px] grid place-items-center border border-amethyst large:w-10 large:h-10 w-8 h-8 rounded-full">
+        <div className="text-amethyst body-1 grid place-items-center border border-amethyst large:w-10 large:h-10 w-8 h-8 rounded-full">
           {props.icon}
         </div>
         <p className="header-4 ml-4 font-bold">{props.title}</p>
