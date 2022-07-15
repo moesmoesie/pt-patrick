c8d3b9bba61b8623ce49d639666df770db47b06f
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-07-15 19:03:30 +0200

    Fix fontawesome iconsize jump

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import "../styles/typography.css";
 import "../styles/globals.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
