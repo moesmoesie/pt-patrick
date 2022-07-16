fca1e3e3d583f8f969bc77466b20cbc9e2ee70cf
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-07-16 20:16:56 +0200

    Update util schema function

--- utils/generateSchemas.js
+++ utils/generateSchemas.js
@@ -1,7 +1,7 @@
 var glob = require("glob");
 var fs = require("fs");
 
-glob("lib/**/schema.ts", function (er, files) {
+glob("lib/**/schema.{ts,tsx}", function (er, files) {
   var content = files.map((el, index) => {
     return `export { default as schema${index} } from "./${el.substring(
       0,
