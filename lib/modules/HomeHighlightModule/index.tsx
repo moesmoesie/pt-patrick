b62879f1496fd4de7d46594b9aa6433312950a7b
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-07-16 09:55:42 +0200

    Make highlight module resposnive

--- lib/modules/HomeHighlightModule/index.tsx
+++ lib/modules/HomeHighlightModule/index.tsx
@@ -5,25 +5,33 @@ const HomeHighlightModudle: React.FC<HomeHighlightModuleProps> = (props) => {
   return (
     <ModuleContainer
       color="#000000"
-      paddingTop={props.paddingTop}
-      paddingBottom={props.paddingBottom}
+      paddingTop={"none"}
+      paddingBottom={"none"}
       hasSeperator={props.hasSeperator}
     >
-      <Image
-        type="sanity"
-        alt={props.image.alt ?? undefined}
-        caption={props.image.caption ?? undefined}
-        asset={props.image.asset}
-        width={500}
-        objectFit="cover"
-        className="h-[355px] w-full -translate-y-10 overflow-hidden rounded-lg object-cover"
-      />
-      <p className="header-2 mb-10 font-bold text-white">{props.title}</p>
-      <p className="body-1 mb-10 text-white">{props.body}</p>
-      <button className="border border-white px-4 py-1 font-rubik font-medium text-white">
-        <FontAwesomeIcon className="text-[14px]" icon="play" />
-        <span className="ml-2 underline">Watch Video</span>
-      </button>
+      <div className="large:flex">
+        <div className="relative flex-[1.3]">
+          <Image
+            type="sanity"
+            alt={props.image.alt ?? undefined}
+            caption={props.image.caption ?? undefined}
+            asset={props.image.asset}
+            width={1000}
+            objectFit="cover"
+            className="h-[375px] w-full -translate-y-12 overflow-hidden rounded-lg object-cover large:absolute large:-left-16 large:-top-[8%] large:h-[116%] large:translate-y-0"
+          />
+        </div>
+        <div className="flex-1 pb-20 large:py-28">
+          <p className="header-2 mb-10 font-bold text-white">{props.title}</p>
+          <p className="body-1 mb-10 text-white large:max-w-[500px]">
+            {props.body}
+          </p>
+          <button className="border border-white px-4 py-1 font-rubik font-medium text-white">
+            <FontAwesomeIcon className="text-[14px]" icon="play" />
+            <span className="ml-2 underline">Watch Video</span>
+          </button>
+        </div>
+      </div>
     </ModuleContainer>
   );
 };
