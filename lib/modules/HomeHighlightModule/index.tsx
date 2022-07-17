938abd5bbad5471903ae4a7ce6e4cb77f2bc2f3a
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-07-17 23:56:41 +0200

    Make highlight module tablet friendly

--- lib/modules/HomeHighlightModule/index.tsx
+++ lib/modules/HomeHighlightModule/index.tsx
@@ -17,16 +17,16 @@ const HomeHighlightModudle: React.FC<HomeHighlightModuleProps> = (props) => {
             alt={props.image.alt ?? undefined}
             caption={props.image.caption ?? undefined}
             asset={props.image.asset}
-            width={1000}
+            width={1200}
             objectFit="cover"
-            className="z-50 h-[375px] w-full -translate-y-12 overflow-hidden rounded-lg object-cover large:absolute large:-left-16 large:-top-[8%] large:h-[116%] large:translate-y-0"
+            className="z-50 h-[375px] w-full -translate-y-12 overflow-hidden rounded-lg object-cover medium:h-[400px] large:absolute large:-left-16 large:-top-[8%] large:h-[116%] large:translate-y-0"
           />
         </div>
         <div className="flex-1 pb-20 large:py-28">
           <h2 className="header-2 mb-10 font-bold text-white">
             {<RichText content={props.title} />}
           </h2>
-          <p className="body-1 mb-10 text-white large:max-w-[500px]">
+          <p className="body-1 mb-10 text-white medium:max-w-2xl large:max-w-[500px]">
             {props.body}
           </p>
           <button className="border border-white px-4 py-1 font-rubik font-medium text-white">
