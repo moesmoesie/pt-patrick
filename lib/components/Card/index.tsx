170a148518bde68879c85e870d9b78d2486f6ca5
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-07-17 23:06:38 +0200

    Reduce blur on cards

--- lib/components/Card/index.tsx
+++ lib/components/Card/index.tsx
@@ -2,7 +2,7 @@ import CardProps from "./types";
 
 const Card: React.FC<CardProps> = (props) => {
   return (
-    <div className="shadow-1 rounded-[20px] border border-amethyst bg-white px-4 py-3 supports-backdrop-filter:bg-white/60 supports-backdrop-filter:backdrop-blur-lg large:px-5 large:py-6">
+    <div className="shadow-1 rounded-[20px] border border-amethyst bg-white px-4 py-3 supports-backdrop-filter:bg-white/40 supports-backdrop-filter:backdrop-blur-md large:px-5 large:py-6">
       <div className="mb-4 flex items-center">
         <div className="body-1 grid h-8 w-8 place-items-center rounded-full border border-amethyst text-amethyst large:h-10 large:w-10">
           {props.icon}
