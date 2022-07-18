b14c67e459e902838fc3d66ca6ced3bc30083158
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-07-18 07:47:56 +0200

    Make contact section more responsive to tablet

--- lib/modules/ContactModule/index.tsx
+++ lib/modules/ContactModule/index.tsx
@@ -11,24 +11,24 @@ const ContactModule: React.FC<ContactModuleProps> = (props) => {
       hasSeperator={props.hasSeperator}
       color="#000000"
     >
-      <div className="mt-10 text-white large:flex large:gap-28">
+      <div className="mt-10 text-white large:flex large:gap-16">
         <div>
           <Image
             alt={props.image.alt ?? undefined}
             caption={props.image.caption ?? undefined}
             asset={props.image.asset}
             type={"sanity"}
-            width={600}
+            width={1200}
             objectFit="cover"
-            className="h-[355px] w-full -translate-y-10 overflow-hidden rounded-lg object-cover large:h-full large:min-w-[650px]"
+            className="h-[355px] w-full -translate-y-10 overflow-hidden rounded-lg object-cover medium:h-[400px] large:h-full large:min-w-[650px]"
           />
         </div>
 
         <div className="large:py-28">
-          <h2 className="header-2 mb-8 pr-4 font-bold">
+          <h2 className="header-2 mb-8 pr-4 font-bold medium:max-w-2xl">
             <RichText content={props.title} />
           </h2>
-          <p className="body-1 mb-7">{props.body}</p>
+          <p className="body-1 mb-7 medium:max-w-2xl">{props.body}</p>
           <div className="flex gap-5">
             <button className="grid h-11 w-11 place-items-center rounded-full border border-white">
               <FontAwesomeIcon
